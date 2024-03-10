f9f3430b14d5cb9b1f3280ec1db06ed61e90735d
shreyasmc <shreyasmc2000@gmail.com>
2024-03-10 08:12:17 +0530

    committed

---
+++ Casing.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Condionalstatements {
+
+	public static void main(String[] args) {
+		int num=12340;
+		while(num>0) {
+			int rem=num%10;
+			System.out.print(rem);
+			num=num/10;
+		}
+
+
+}}
+
